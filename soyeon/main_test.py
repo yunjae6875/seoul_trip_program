ca58dbc02e8607d14272b3c0c58ddb7711591d0e
Soyeon Bak <bluewaters1104@gmail.com>
2023-07-05 01:22:54 +0900

    0705 main_test.py 로 옮겨서 클릭이벤트 추가중

--- soyeon/main_test.py
+++ soyeon/main_test.py
@@ -6,41 +6,40 @@ from PyQt5 import uic
 from soyeon.test import *
 from PyQt5.QtGui import *
 
+
 def resource_path(relative_path):
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
+
 form = resource_path('seoul_main_page.ui')
 form_class = uic.loadUiType(form)[0]
 
+
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
-        super( ).__init__( )
+        super().__init__()
         self.setupUi(self)
 
-        self.insert_values_in_gridlayout()
+        self.insert_values_in_gridlayout() # 그리드 레이아웃에 값 넣기
 
-        # self.widget.setLayout(grid)
+        # 라벨 클릭하면 오픈 페이지로 이동
+        self.opening_page_img_lab.mousePressEvent = lambda event: self.stackedWidget.setCurrentWidget(self.main_page_1)
 
-    # hover 하면 색 변하게 하기 (수정필요)
+
     def insert_values_in_gridlayout(self):
-        # self.button_group = QButtonGroup()  # 버튼 그룹 생성
-        cnt = 0
-        for i in range(1, 6):
-            for j in range(1, 6):
-                button = QPushButton(gu_list[cnt])  # 버튼 생성 및 이름 넣어줌
-                button.setFixedSize(100, 100)  # 버튼의 크기 고정
-                button.setCheckable(True)  # 선택할 수 있게 설정
-                button.setStyleSheet('''
-                border-radius:15px;
-                border: 1px solid black;
-                background-color: rgb(255, 255, 255);
-                ''')
-                # self.button_group.addButton(button)  # 버튼 그룹에 버튼 추가
-                self.gridLayout.addWidget(button, i, j)
-                cnt += 1
-
-
+        """그리드 레이아웃에 값 넣기"""
+        btn_style = '''
+        border-radius:15px;
+        border: 1px solid black;
+        background-color: rgb(255, 255, 255);
+        '''
+        for i, gu in enumerate(gu_list, start=1):
+            button = QPushButton(gu)  # 버튼 생성 및 이름 넣어줌
+            button.setFixedSize(100, 100)  # 버튼의 크기 고정
+            button.setStyleSheet(btn_style)  # 버튼 스타일 설정
+            self.gridLayout.addWidget(button, (i - 1) // 5 + 1, (i - 1) % 5 + 1)  # 그리드 레이아웃에 버튼 위치 설정 (행위치 / 열위치)
+            # print((i - 1) // 5 + 1, (i - 1) % 5 + 1)
 
 
 if __name__ == '__main__':
@@ -49,6 +48,6 @@ if __name__ == '__main__':
     fontDB.addApplicationFont('./font/Pretendard-Medium.ttf')
     app.setFont(QFont('Pretendard Medium'))
 
-    myWindow = WindowClass( )
-    myWindow.show( )
-    app.exec_( )
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
