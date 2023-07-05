bd735ab347cd3bdec1d4d4b84f66a4ce8e432c12
yunjae6875 <rladbswo1816@gmail.com>
2023-07-05 20:11:59 +0900

    7/5(수) 20:11 - Map

--- yunjae/folium_map_1.py
+++ yunjae/folium_map_1.py
@@ -1,7 +1,7 @@
 """
 작성자: 김윤재
 최초작성: 2023-07-03(월)
-최종수정: 2023-07-05(수) 19:32
+최종수정: 2023-07-05(수) 20:31
 """
 
 # --- import modules
@@ -47,8 +47,9 @@ class FoliumMap(QWidget):
         self.button.clicked.connect(self.button_clicked_event)
 
         # --- self.seoul_map 지도 설정
-        self.latitude = 37.394946  # 위도
-        self.longitude = 127.111104  # 경도
+        self.latitude = 37.564214  # 위도
+        self.longitude = 127.001699  # 경도
+        self.zoom_level = 11
         self.titles = "http://mt0.google.com/vt/lyrs=m&hl=ko&x={x}&y={y}&z={z}"
         self.attr = "Google"
         # self.coordinate = (35.19475778198943, 126.8399771747554)
@@ -57,7 +58,7 @@ class FoliumMap(QWidget):
         self.seoul_map = folium.Map(
             tiles=self.titles,  # --- 배경지도 tiles에 대한 속성 (기본값: https://www.openstreetmap.org)
             attr=self.attr,
-            zoom_start=10,  # --- 화면 보여줄 거리 / 값이 적을수록 가까이 보여줌
+            zoom_start=self.zoom_level,  # --- 화면 보여줄 거리 / 값이 적을수록 가까이 보여줌
             location=[self.latitude, self.longitude],  # --- 현재 화면에 보여줄 좌표 값
             control_scale=True,  # --- contol_scale: True 시 맵 좌측 하단에 배율을 보여줌
             # zoom_control = False,   # --- zoom_control: False 시 줌 컨트롤러가 사라집니다. (단, 마우스 휠 줌은 가능)
@@ -71,62 +72,92 @@ class FoliumMap(QWidget):
         """
         ▼ 메소드 설명:
         self.mapping_tour_all_show()  -> 모든 관광명소를 지도에 마커+클러스트로 표시합니다.
+        self.mapping_tour_guname_show()  -> 특정 관광명소를 지도에 마커+클러스트로 표시합니다.
         self.mapping_lodges_all_show()  -> 모든 숙박업소를 지도에 마커+클러스트로 표시합니다.
+        self.mapping_lodges_guname_show()  -> 특정 숙박업소를 지도에 마커+클러스트로 표시합니다.
         self.mapping_food_all_show()  -> 모든 음식점을 지도에 마커+클러스트로 표시합니다.
-        self.mapping_food_guname_show(guname: str)  -> food_list DB의 'gu_name' 컬럼의 레코드를 입력하면 특정 구만 검색하여 표시합니다.  
+        self.mapping_food_guname_show(guname: str)  -> 특정 음식점을 지도에 마커+클러스트로 표시합니다.  
         """
-        self.mapping_tour_all_show()
         self.load_map()  # --- 현재 폴더에 index.html 파일을 저장하고, index.html 파일 불러오기
 
     # --- 메소드
     def mapping_tour_all_show(self):
         """DB의 관광명소 목록을 맵에 마커 + 클러스트로 적용시킵니다"""
-        tour_query = pd.read_sql("SELECT 상호명, 신주소, 전화번호, 웹사이트, x_pos, y_pos FROM seoul_tourist", self.con)
+        tour_query = pd.read_sql("SELECT 상호명, 신주소, 전화번호, 웹사이트, x_pos, y_pos, img_path FROM seoul_tourist", self.con)
         for index, row in tour_query.iterrows():
             x_pos = row['x_pos']
             y_pos = row['y_pos']
             name = row['상호명']
             info = row['신주소'], row['전화번호']
             link = f"<a href={row['웹사이트']}>웹사이트 접속</a>"
-            roadview = '<a href="https://www.google.com/maps?layer=c&cbll=' + str(x_pos) + ',' + str(y_pos) + ' target="_blank">GOOGLE STREET VIEW</a>'
-            # popup = folium.Popup(name + f"({str(link)})" + "<br><br>" + str(info), min_width=500, max_width=500)
-            popup = folium.Popup(name + f"({str(link)})" + "<br><br>" + str(info) + "<br><br>" + roadview, min_width=500, max_width=500)
+            img = row['img_path']
+            roadview = '<a href="https://www.google.com/maps?layer=c&cbll=' + str(x_pos) + ',' + str(y_pos) + '" target="_blank">GOOGLE STREET VIEW(Mouse Wheel Click)</a>'
+            popup = folium.Popup(f"<img src='{img}'>" + "<br>" + name + f"({str(link)})" + "<br><br>" + str(info) + "<br><br>" + roadview, min_width=500, max_width=500)
+            folium.Marker([x_pos, y_pos], tooltip=name, popup=popup, icon=folium.Icon(color="red")).add_to(self.marker_cluster)
+
+    def mapping_tour_guname_show(self, guname: str):
+        """DB의 관광명소 목록을 맵에 마커 + 클러스트로 적용시킵니다"""
+        tour_query = pd.read_sql(f"SELECT 상호명, 신주소, 전화번호, 웹사이트, x_pos, y_pos, img_path FROM seoul_tourist WHERE 신주소 LIKE '%{guname}%'", self.con)
+        for index, row in tour_query.iterrows():
+            x_pos = row['x_pos']
+            y_pos = row['y_pos']
+            name = row['상호명']
+            info = row['신주소'], row['전화번호']
+            link = f"<a href={row['웹사이트']}>웹사이트 접속</a>"
+            img = row['img_path']
+            roadview = '<a href="https://www.google.com/maps?layer=c&cbll=' + str(x_pos) + ',' + str(y_pos) + '" target="_blank">GOOGLE STREET VIEW(Mouse Wheel Click)</a>'
+            popup = folium.Popup(f"<img src='{img}'>" + "<br>" + name + f"({str(link)})" + "<br><br>" + str(info) + "<br><br>" + roadview, min_width=500, max_width=500)
             folium.Marker([x_pos, y_pos], tooltip=name, popup=popup, icon=folium.Icon(color="red")).add_to(self.marker_cluster)
 
     def mapping_lodges_all_show(self):
         """DB의 숙박지 목록을 맵에 마커 + 클러스트로 적용시킵니다"""
-        lodge_query = pd.read_sql("SELECT 사업장명, 도로명주소, 전화번호, x_pos, y_pos FROM seoul_lodges", self.con)
+        lodge_query = pd.read_sql("SELECT 사업장명, 도로명주소, 전화번호, x_pos, y_pos, img_path FROM seoul_lodges", self.con)
         for index, row in lodge_query.iterrows():
             x_pos = row['x_pos']
             y_pos = row['y_pos']
             name = row['사업장명']
             info = row['도로명주소'], row['전화번호']
-            popup = folium.Popup(name + "<br>" + str(info), min_width=400, max_width=400)
+            img = row['img_path']
+            roadview = '<a href="https://www.google.com/maps?layer=c&cbll=' + str(x_pos) + ',' + str(y_pos) + '" target="_blank">GOOGLE STREET VIEW(Mouse Wheel Click)</a>'
+            popup = folium.Popup(f"<img src='{img}'>" + "<br><br>" + name + "<br><br>" + str(info) + "<br><br>" + roadview, min_width=400, max_width=400)
+            folium.Marker([x_pos, y_pos], tooltip=name, popup=popup, icon=folium.Icon(color="blue")).add_to(self.marker_cluster)
+
+    def mapping_lodges_guname_show(self, guname: str):
+        """DB의 숙박지 목록을 구별로 맵에 마커 + 클러스트로 적용시킵니다"""
+        lodge_query = pd.read_sql(f"SELECT 사업장명, 도로명주소, 전화번호, x_pos, y_pos, img_path FROM seoul_lodges WHERE 도로명주소 LIKE '%{guname}%'", self.con)
+        for index, row in lodge_query.iterrows():
+            x_pos = row['x_pos']
+            y_pos = row['y_pos']
+            name = row['사업장명']
+            info = row['도로명주소'], row['전화번호']
+            img = row['img_path']
+            roadview = '<a href="https://www.google.com/maps?layer=c&cbll=' + str(x_pos) + ',' + str(y_pos) + '" target="_blank">GOOGLE STREET VIEW(Mouse Wheel Click)</a>'
+            popup = folium.Popup(f"<img src='{img}'>" + "<br><br>" + name + "<br><br>" + str(info) + "<br><br>" + roadview, min_width=400, max_width=400)
             folium.Marker([x_pos, y_pos], tooltip=name, popup=popup, icon=folium.Icon(color="blue")).add_to(self.marker_cluster)
 
     def mapping_food_all_show(self):
         """DB의 음식점 목록을 맵에 마커 + 클러스트로 적용시킵니다"""
         food_query = pd.read_sql("SELECT name, address, x_pos, y_pos, img_path FROM food_list", self.con)
-        for index, row in self.df_food.iterrows():
+        for index, row in food_query.iterrows():
+            x_pos = row['x_pos']
+            y_pos = row['y_pos']
+            name = row['name']
+            info = row['address']
+            img = row['img_path']
+            roadview = '<a href="https://www.google.com/maps?layer=c&cbll=' + str(x_pos) + ',' + str(y_pos) + '" target="_blank">GOOGLE STREET VIEW(Mouse Wheel Click)</a>'
+            popup = folium.Popup(f"<img src='{img}'>" + "<br><br>" + name + "<br><br>" + str(info) + "<br><br>" + roadview, min_width=400, max_width=400)
+            folium.Marker([x_pos, y_pos], tooltip=name, popup=popup, icon=folium.Icon(color="red")).add_to(self.marker_cluster)
+
+    def mapping_food_guname_show(self, guname: str):
+        """DB의 음식점 목록을 구별로 마커 + 클러스트로 적용시킵니다"""
+        food_query = pd.read_sql(f"SELECT gu_name, name, rate, address, x_pos, y_pos, img_path FROM food_list WHERE gu_name = '{guname}'", self.con)
+        for index, row in food_query.iterrows():
             x_pos = row['x_pos']
             y_pos = row['y_pos']
             name = row['name']
             info = row['address']
             img = row['img_path']
             popup = folium.Popup(f"<img src='{img}'>" + "<br><br>" + name + "<br><br>" + str(info), min_width=400, max_width=400)
-            folium.Marker([x_pos, y_pos], tooltip=name, popup=popup, icon=folium.Icon(color="red")).add_to(self.marker_cluster)
-
-    def mapping_food_guname_show(self, guname: str):
-        """DB의 음식점 목록을 '구'별로 마커 + 클러스트로 적용시킵니다"""
-        read_guname = pd.read_sql(f"SELECT gu_name, name, rate, address, x_pos, y_pos FROM food_list WHERE gu_name = '{guname}'", self.con)
-        for index, row in read_guname.iterrows():
-            x_pos = row['x_pos']
-            y_pos = row['y_pos']
-            name = row['name']
-            info = row['address']
-            # img = row['img_path']
-            # popup = folium.Popup(f"<img src='{img}'>" + "<br><br>" + name + "<br><br>" + str(info), min_width=400, max_width=400)
-            popup = folium.Popup(name + "<br><br>" + str(info), min_width=400, max_width=400)
             folium.Marker([x_pos, y_pos], tooltip=name, popup=popup, icon=folium.Icon(color="red")).add_to(self.marker_cluster)
 
     def load_map(self):
@@ -145,7 +176,7 @@ class FoliumMap(QWidget):
             self.webEngineView.setUrl(QUrl(html))
 
     def button_clicked_event(self):
-        """뒤로가기 버튼"""
+        """구글 스트릿 뷰 상태에서 뒤로가기 버튼을 클릭하면 원래 화면으로 이동합니다"""
         self.web.page().triggerAction(QWebEnginePage.WebAction.Back)
         pass
 
