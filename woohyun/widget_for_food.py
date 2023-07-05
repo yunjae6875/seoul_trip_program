f0e251e6064cf9dcc721167f6015700cc5e980e4
노우현 <sdew301@naver.com>
2023-07-05 10:53:43 +0900

    UI 이미지 스케일링 및 클래스 파일 추가

--- woohyun/widget_for_food.py
+++ woohyun/widget_for_food.py
@@ -1,30 +1,30 @@
 import sys
-from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from seoul_widget import *
 
-class SeoulWidget(QWidget, Ui_Form):
-    def __init__(self, name, address, main_dishes, img_path, parent=None):
+class SeoulForFood(QWidget, Ui_Form):
+    def __init__(self, name, rate, address, main_dishes, price, img_path, parent=None):
         super().__init__(parent)
         self.setupUi(self)
 
         self.name = name
+        self.rate = rate
         self.address = address
         self.main_dishes = main_dishes
+        self.price = price
         self.img_path = img_path
 
         self.parent_seoul = parent
-
-        self.name_lab.setText(f"이 름 : {self.name}")
-        self.type_lab.setText(f"메인 메뉴 : {self.main_dishes}")
+        self.name_lab.setText(f"이 름 : {self.name}   평 점 : {self.rate}")
+        self.type_lab.setText(f"메인 메뉴 : {self.main_dishes}   가 격 대 : {self.price}")
         self.location_lab.setText(f"주 소 : {self.address}")
         self.img_label.setPixmap(QPixmap(f"{self.img_path}"))
 
     def mousePressEvent(self, event):
         self.parent_seoul.stackedWidget.setCurrentWidget(self.parent_seoul.main_page_4)
-        self.parent_seoul.name_lab.setText(f"이 름 : {self.name}")
-        self.parent_seoul.type_lab.setText(f"메인 메뉴 : {self.main_dishes}")
+        self.parent_seoul.name_lab.setText(f"이 름 : {self.name}     평 점 : {self.rate}")
+        self.parent_seoul.type_lab.setText(f"메인 메뉴 : {self.main_dishes}     가 격 대 : {self.price}")
         self.parent_seoul.location_lab.setText(f"주 소 : {self.address}")
         self.parent_seoul.img_label.setPixmap(QPixmap(f"{self.img_path}"))
 
@@ -32,7 +32,7 @@ class SeoulWidget(QWidget, Ui_Form):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    myWindow = SeoulWidget()
+    myWindow = SeoulForFood()
     myWindow.show()
     app.exec()
 
